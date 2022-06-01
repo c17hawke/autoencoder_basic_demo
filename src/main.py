2f0026db9b771bdbd94af7740491ebbc27fac35b
c17hawke <sunny.c17hawke@gmail.com>
2022-06-01 18:37:11 +0530

    final done

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from pickletools import optimize
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -12,8 +13,24 @@ def generate_data(DATA_POINTS=60, COLUMNS=3, SEED=42):
     X += 0.28 * np.random.randn(DATA_POINTS, COLUMNS)  # adding more noise
     return X + [0.3, 0, 0.3]
 
-def get_model(X_train):
-    pass
+def get_prediction(X_train, SEED=42, LR=0.5, EPOCHS=500):
+    tf.random.set_seed(SEED)
+
+    encoder = tf.keras.Sequential([
+        tf.keras.layers.Dense(2)
+    ])
+    decoder = tf.keras.Sequential([
+        tf.keras.layers.Dense(3)
+    ])
+
+    autoencoder = tf.keras.Sequential([encoder, decoder])
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=LR)
+    autoencoder.compile(loss="mse", optimizer=optimizer)
+
+    history = autoencoder.fit(X_train, X_train, epochs=EPOCHS)
+    return encoder.predict(X_train)
+
 
 if __name__ == '__main__':
     X_train = generate_data()
@@ -23,4 +40,11 @@ if __name__ == '__main__':
     fig = px.scatter_3d(df, x='x', y='y', z='z', opacity=0.7)
     fig.show()
 
-    enocdings = get_model(X_train)
+    encodings = get_prediction(X_train)
+    pred_df = pd.DataFrame(np.c_[encodings, np.zeros(encodings.shape[0])], columns=["new_x", "new_y", "None"])
+
+    print(f"description of pred_df:\n{pred_df.describe().T}")
+
+    fig = px.scatter_3d(pred_df, x='new_x', y='new_y', z='None', opacity=0.7)
+    fig.show()
+
